afd02b02fa827935c76157c8ea8cb0e5227f0de2
Shr4528 <Shr4528@gmail.com>
2023-02-14 16:08:01 +0200

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+//import data 
+import { banner } from '../data'
+
 const Banner = () => {
-  return <div>Banner</div>;
+  // destructure banner data
+  const { titlePart1, titlePart2, subtitle, textBtn } = banner;
+
+  return (
+    <section className='bg-neutral-500 h-[790px]'>
+      <div className='container mx-auto h-full'>
+        <div className='flex items-center h-full relative -space-x-48 lg:-space-x-24'>
+          {/* text */}
+          <div className='text-white flex-1 z-10 pl-8 lg:pl-0'>
+            <h1 className='h1 text-white mb-8'>
+              {titlePart1} <br />
+              <span className='text-primary-200'>{titlePart2}</span>
+            </h1>
+            <p className='max-w-[415px] text-body-mb lg:text-body-lg'>{subtitle}</p>
+            <button className='btn btn-sm lg:btn-lg btn-secondary'>{textBtn}</button>
+          </div>
+          {/* image */}
+          <div className='bg-blue-400 w-full h-full bg-banner bg-cover 
+          bg-right lg:bg-center bg-no-repeat flex-1'>Image</div>
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Banner;
