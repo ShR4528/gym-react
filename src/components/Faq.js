baf05e54c030576d7b01b474e310be504139534d
Shr4528 <Shr4528@gmail.com>
2023-02-18 12:52:32 +0200

    join

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -10,7 +10,7 @@ const Faq = () => {
   const { icon, title, accordions } = faq;
   return (
     <section className='section pt-[480px]
-    lg:pt-[280px]'>
+    lg:pt-[280px] mb-[80px] lg:mb-0'>
       <div className='max-w-[768px] mx-auto lg:bg-faq
       bg-no-repeat bg-custom bg-center
       lg:h-[1160px]
